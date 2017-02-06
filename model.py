21568654e1c68b2fc5f0cdf70b554d2784074d14
aleneus <aleneus@gmail.com>
2017-02-06 22:07:44 +0300

    mapping

--- model.py
+++ model.py
@@ -45,85 +45,9 @@ class GrouperModel:
         self.conn = None
         self.fname = None
 
-    """ Data Viewing
+    """ Mapping
     """
-
-    def db_info(self):
-        # number of groups
-        q = "\
-        SELECT count(*)\
-        FROM egeg_group\
-        "
-        groups_num = list(self.c.execute(q, []))[0][0]
-        # total number of examinations
-        q = "\
-        SELECT count(*)\
-        FROM examination\
-        "
-        exams_total_num = list(self.c.execute(q, []))[0][0]
-        # fields from db
-        q = "\
-        SELECT *\
-        FROM egeg_group\
-        "
-        fields = list(self.c.execute(q, []))
-        # numbers of exams in groups
-        num_in_groups = []
-        for f in fields:
-            q = "\
-            SELECT COUNT(E.exam_id)\
-            FROM examination as E, group_element as GE\
-            WHERE GE.exam_id = E.exam_id AND GE.group_id = ?\
-            "
-            num_in_groups.append(list(self.c.execute(q, [f[0], ]))[0][0])
-        # number of ungrouped examinations
-        q = "\
-        SELECT COUNT(exam_id)\
-        FROM examination\
-        WHERE exam_id NOT IN (SELECT exam_id FROM group_element)\
-        "
-        ungrouped_num = list(self.c.execute(q, []))[0][0]
-        return [exams_total_num, groups_num, fields, num_in_groups, ungrouped_num]
-
-    def group_info(self, group_id):
-        if group_id == '0':
-            return list(self.c.execute("\
-            SELECT exam_id, name, diagnosis, age, gender\
-            FROM examination\
-            WHERE exam_id NOT IN (SELECT exam_id FROM group_element)\
-            ", []))
-            
-        return list(self.c.execute("\
-        SELECT E.exam_id, E.name, E.diagnosis, E.age, E.gender\
-        FROM examination AS E, group_element AS GE\
-        WHERE GE.exam_id = E.exam_id AND GE.group_id = ?\
-        ORDER BY E.name\
-        ", [group_id, ]))
-
-    def exam_info(self, exam_id):
-        e = list(self.c.execute("\
-        SELECT * FROM examination\
-        WHERE exam_id = ?\
-        ", [exam_id, ]))[0]
-        s = ('234', 'source', '40 m', '2 Hz', 'Q=0.56')
-        ms = []
-        ms_sql = list(self.c.execute("\
-        SELECT meas_id, time FROM measurement\
-        WHERE exam_id = ?\
-        ORDER BY meas_id\
-        ", [exam_id, ]))
-        for m in ms_sql:
-            ss = []
-            ss = list(self.c.execute("\
-            SELECT signal_id, edited FROM signal\
-            WHERE meas_id = ?\
-            ORDER BY edited\
-            ", [m[0], ]))
-            ms.append((m, ss))
-     
-        res = (e, ms)
-        return res
-
+    
     def get_examination(self, exam_id):
         e = Examination()
         e.name, e.diagnosis, e.age, e.gender = list(self.c.execute("""
@@ -149,6 +73,122 @@ class GrouperModel:
                 m.ss.append(s)
             e.ms.append(m)
         return e
+
+    def insert_examination(self, e):
+        exam_id = list(self.c.execute("""
+        SELECT max(exam_id)
+        FROM examination
+        """))[0][0]
+        if not exam_id:
+            exam_id = 0
+            
+        meas_id = list(self.c.execute("""
+        SELECT max(meas_id)
+        FROM measurement
+        """))[0][0]
+        if not meas_id:
+            meas_id = 0
+
+        self.c.execute("""
+        INSERT INTO examination (name, diagnosis, age, gender)
+        VALUES (?,?,?,?)
+        """, (e.name, e.diagnosis, e.age, e.gender) )
+        exam_id += 1
+
+        for m in e.ms:
+            self.c.execute("""
+            INSERT INTO measurement (time, exam_id)
+            VALUES (?,?)
+            """, (m.time, exam_id) )
+            meas_id += 1
+
+            for s in m.ss:
+                self.c.execute("""
+                INSERT INTO signal (data, dt, edited, meas_id)
+                VALUES (?,?,?,?)
+                """, (ndarry2blob(s.x), s.dt, 0, meas_id) )
+            
+        self.conn.commit()
+
+    """ Data Viewing
+    """
+
+    def db_info(self):
+        # number of groups
+        q = """
+        SELECT count(*)
+        FROM egeg_group
+        """
+        groups_num = list(self.c.execute(q, []))[0][0]
+        # total number of examinations
+        q = """
+        SELECT count(*)
+        FROM examination
+        """
+        exams_total_num = list(self.c.execute(q, []))[0][0]
+        # fields from db
+        q = """
+        SELECT *
+        FROM egeg_group
+        """
+        fields = list(self.c.execute(q, []))
+        # numbers of exams in groups
+        num_in_groups = []
+        for f in fields:
+            q = """
+            SELECT COUNT(E.exam_id)
+            FROM examination as E, group_element as GE
+            WHERE GE.exam_id = E.exam_id AND GE.group_id = ?
+            """
+            num_in_groups.append(list(self.c.execute(q, [f[0], ]))[0][0])
+        # number of ungrouped examinations
+        q = """
+        SELECT COUNT(exam_id)
+        FROM examination
+        WHERE exam_id NOT IN (SELECT exam_id FROM group_element)
+        """
+        ungrouped_num = list(self.c.execute(q, []))[0][0]
+        return [exams_total_num, groups_num, fields, num_in_groups, ungrouped_num]
+
+    def group_info(self, group_id):
+        if group_id == '0':
+            return list(self.c.execute("""
+            SELECT exam_id, name, diagnosis, age, gender
+            FROM examination
+            WHERE exam_id NOT IN (SELECT exam_id FROM group_element)
+            """, []))
+            
+        return list(self.c.execute("""
+        SELECT E.exam_id, E.name, E.diagnosis, E.age, E.gender
+        FROM examination AS E, group_element AS GE
+        WHERE GE.exam_id = E.exam_id AND GE.group_id = ?
+        ORDER BY E.name
+        """, [group_id, ]))
+
+    def exam_info(self, exam_id):
+        e = list(self.c.execute("""
+        SELECT * FROM examination
+        WHERE exam_id = ?
+        """, [exam_id, ]))[0]
+        s = ('234', 'source', '40 m', '2 Hz', 'Q=0.56')
+        ms = []
+        ms_sql = list(self.c.execute("""
+        SELECT meas_id, time FROM measurement
+        WHERE exam_id = ?
+        ORDER BY meas_id
+        """, [exam_id, ]))
+        for m in ms_sql:
+            ss = []
+            ss = list(self.c.execute("""
+            SELECT signal_id, edited FROM signal
+            WHERE meas_id = ?
+            ORDER BY edited
+            """, [m[0], ]))
+            ms.append((m, ss))
+     
+        res = (e, ms)
+        return res
+
         
     """ Grouping
     """
@@ -218,40 +258,7 @@ class GrouperModel:
 
     def add_exam_from_json_folder(self, folder_name):
         e = get_exam_from_folder(folder_name)
-        exam_id = list(self.c.execute("""
-        SELECT max(exam_id)
-        FROM examination
-        """))[0][0]
-        if not exam_id:
-            exam_id = 0
-            
-        meas_id = list(self.c.execute("""
-        SELECT max(meas_id)
-        FROM measurement
-        """))[0][0]
-        if not meas_id:
-            meas_id = 0
-
-        self.c.execute("""
-        INSERT INTO examination (name, diagnosis, age, gender)
-        VALUES (?,?,?,?)
-        """, (e.name, e.diagnosis, e.age, e.gender) )
-        exam_id += 1
-
-        for m in e.ms:
-            self.c.execute("""
-            INSERT INTO measurement (time, exam_id)
-            VALUES (?,?)
-            """, (m.time, exam_id) )
-            meas_id += 1
-
-            for s in m.ss:
-                self.c.execute("""
-                INSERT INTO signal (data, dt, edited, meas_id)
-                VALUES (?,?,?,?)
-                """, (ndarry2blob(s.x), s.dt, 0, meas_id) )
-            
-        self.conn.commit()
+        self.insert_examination(e)
 
     def export_as_json_folder(self, exam_id, folder_name):
         e = self.get_examination(exam_id)
