ea28126a791f2cc98a893a07502faeee167a43dd
aleneus <aleneus@gmail.com>
2017-02-06 23:03:40 +0300

    add cmd test

---
+++ igrouper.py
@@ -0,0 +1,16 @@
+import cmd, sys
+
+class GrouperShell(cmd.Cmd):
+    intro = 'Welcome to the Grouper shell.   Type help or ? to list commands.\n'
+    prompt = 'igrouper> '
+
+    def do_test(self, arg):
+        'Help string for test command'
+        print(arg.split())
+
+    def do_q(self, arg):
+        'exit igrouper'
+        return True
+
+if __name__ == '__main__':
+    GrouperShell().cmdloop()
