346771363f6c0c780ba5df8b4afd9f44ac2a0e3d
aleneus <aleneus@gmail.com>
2017-02-11 13:09:53 +0300

    add abstract view class

---
+++ view.py
@@ -0,0 +1,19 @@
+class View:
+    def message(self, text):
+        pass
+        
+    def error_message(self, text):
+        pass
+    
+    def row(self, data):
+        pass
+
+    def table(self, data):
+        pass
+
+    def db(self, db_info):
+        pass
+
+    def exam(self, e_info):
+        pass
+    
