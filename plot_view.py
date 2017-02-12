6cebad2a04dd207270c8322cdd505a403066d1fb
aleneus <aleneus@gmail.com>
2017-02-12 23:16:05 +0300

    views

--- plot_view.py
+++ plot_view.py
@@ -1,7 +1,4 @@
 import matplotlib.pyplot as plt
-
-from pylab import rcParams
-
 from egegrouper_mvc.view import View
 
 class PlotView(View):
@@ -12,14 +9,16 @@ class PlotView(View):
                 n = n + 1
                 
         i = 0
+        #plt.suptitle('{} {} {} {}\n'.format(e.name, e.gender, e.age, e.diagnosis))
         for m in e.ms:
-            # t = 'source'
+            t = m.time
             for s in m.ss:
                 i = i + 1
                 plt.subplot(n*100 + 10 + i)
                 plt.plot(s.x)
                 plt.xlim(0,len(s.x))
                 plt.grid(True)
+                plt.title(t)
 
         plt.tight_layout()
         plt.show()
