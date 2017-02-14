fac17456f9d713895e4457463aad7f7b4625495b
aleneus <aleneus@gmail.com>
2017-02-14 17:26:47 +0300

    tabular

--- egegrouper_mvc/view_string.py
+++ egegrouper_mvc/view_string.py
@@ -9,7 +9,7 @@ class ViewString(View):
         s = 'Error: {}'.format(text)
         return s
     
-    def table(self, data, headers = None, width=30):
+    def table(self, data, headers = None, width=40):
         t = []
         for row in data:
             t_row = []
@@ -22,17 +22,23 @@ class ViewString(View):
             return '\n' + tabulate(t, tablefmt="orgtbl") + '\n'
 
     def exam(self, e):
-        s = 'E: {} {} {} {}\n'.format(e.name, e.gender, e.age, e.diagnosis)
+        s = '\nE: {} {} {} {}\n'.format(e.name, e.gender, e.age, e.diagnosis)
         for m in e.ms:
             s += '    M: {}\n'.format(m.time)
         return s
     
     def storage(self, db_info):
-        s = '\n'
-        s += 'Total examinations number: {}\n'.format(db_info[0])
-        s += 'Groups number: {}\n'.format(db_info[1])
-        for gi, gn in zip(db_info[2],db_info[3]):
-             s += '{} {} ({}): {}\n'.format(gi[0], gi[1], gi[2], gn)
-        s += 'Ungrouped: {}\n'.format(db_info[4])
-        return s
+        # [exams_total_num, groups_num, fields, num_in_groups, ungrouped_num]
+        t = []
+        for row, num in zip(db_info[2], db_info[3]):
+            t_row = []
+            for record in row:
+                t_row.append(str(record))
+            t_row.append(num)
+            t.append(t_row)
+        t.append(['','','',''])
+        t.append(['','','Ungrouped:',db_info[4]])
+        t.append(['','','Total:',db_info[0]])
+        h = ['ID','Name','Description','Number']
+        return '\n' + tabulate(t, headers = h, tablefmt="orgtbl") + '\n'
 
