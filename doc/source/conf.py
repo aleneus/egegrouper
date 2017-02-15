b2cde017c465fc5ac2ecb083281d6562eb185bd2
aleneus <aleneus@gmail.com>
2017-02-15 16:31:44 +0300

    doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,6 +21,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('../egegrouper'))
 sys.path.insert(0, '../egegrouper')
+sys.path.insert(1, '../')
 
 # -- General configuration ------------------------------------------------
 
