53487329026d8daabad6fcf9667688a58299bafc
aleneus <aleneus@gmail.com>
2017-02-18 22:32:21 +0300

    refact

--- egegrouper/edit_record_dialog.py
+++ egegrouper/edit_record_dialog.py
@@ -1,18 +1,7 @@
 import readline
 from collections import OrderedDict
 
-
-class TextDialog:
-    """Base class for text dialog."""
-    def rlineinput(self, prompt, prefill=''):
-        readline.set_startup_hook(lambda: readline.insert_text(prefill))
-        try:
-            return input(prompt)
-        finally:
-            readline.set_startup_hook()
-
-
-class FieldsTextDialog(TextDialog):
+class EditRecordDialog:
     """Text dialog for input fields values."""
     
     data_dict = None
@@ -26,3 +15,10 @@ class FieldsTextDialog(TextDialog):
     def input(self):
         for key in self.data_dict:
             self.data_dict[key] = self.rlineinput(key + ': ', self.data_dict[key])
+            
+    def rlineinput(self, prompt, prefill=''):
+        readline.set_startup_hook(lambda: readline.insert_text(prefill))
+        try:
+            return input(prompt)
+        finally:
+            readline.set_startup_hook()
