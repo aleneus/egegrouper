5b4c4ca465ee6c9bf89ca30fb22b37c1831869e8
aleneus <aleneus@gmail.com>
2017-02-21 15:23:37 +0300

    create widget for storage information

--- egegrouper/views_tk.py
+++ egegrouper/views_tk.py
@@ -4,16 +4,18 @@ from tkinter import ttk
 
 class ViewStorageTk(ViewStorage):
     """Tk view to show common information about storage."""
+    widget = None
     
     def show_data(self):
         """Show information about storage."""
         exams_num, groups_data, num_in_groups, ungrouped_num = self.data
-        self.tree.delete(*self.tree.get_children())
+        tree = self.widget.tree
+        tree.delete(*tree.get_children())
         for (row, num) in zip(groups_data, num_in_groups):
-            self.tree.insert("", END, text=str(row[0]), values=(row[1], row[2], num))
-        self.tree.insert("", END, text="", values=("", "Ungrouped", ungrouped_num) )
-        self.tree.insert("", END, text="", values=("", "Total", exams_num) )
+            tree.insert("", END, text=str(row[0]), values=(row[1], row[2], num))
+        tree.insert("", END, text="", values=("", "Ungrouped", ungrouped_num))
+        tree.insert("", END, text="", values=("", "Total", exams_num))
 
     def set_widget(self, widget):
         """Set widget."""
-        self.tree = widget
+        self.widget = widget
--- tkgrouper.py
+++ tkgrouper.py
@@ -10,6 +10,32 @@ from egegrouper.controller import *
 from egegrouper.model_sqlite3 import *
 from egegrouper.views_tk import *
 
+
+# my widgets
+
+class StorageInfoTable(Frame):
+    """Table widget for stoarge info."""
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.scrollbar = Scrollbar(self, orient=VERTICAL)
+        self.tree = ttk.Treeview(self, yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.tree.yview)
+        self.tree["columns"]=("Name", "Description", "Num")
+        self.tree.heading("#0", text="ID")
+        self.tree.column("#0", width=50)
+        self.tree.column("Name")
+        self.tree.heading("Name", text="Name")
+        self.tree.column("Description")
+        self.tree.heading("Description", text="Description")
+        self.tree.column("Num", width=50)
+        self.tree.heading("Num", text="Num")
+        self.scrollbar.pack(side=RIGHT, fill=Y, expand=1)
+        self.tree.pack()
+        self.pack()
+
+
+# Application
+
 class Application(Frame):
     """Application class."""
 
@@ -32,30 +58,11 @@ class Application(Frame):
         root.config(menu=menubar)
 
     def init_mvc(self):
+        """Initialization of MVC system."""
         self.grouper = GrouperController()
-        self.model = GrouperModelSqlite3()
-        self.grouper.set_model(self.model)
-
-        # tree view for groups list
-        self.frame = Frame(self)
-        self.scrollbar = Scrollbar(self.frame, orient=VERTICAL)
-        self.tree = ttk.Treeview(self.frame, yscrollcommand=self.scrollbar.set)
-        self.scrollbar.config(command=self.tree.yview)
-        self.tree["columns"]=("Name", "Description", "Num")
-        self.tree.heading("#0", text="ID")
-        self.tree.column("#0", width=50)
-        self.tree.column("Name")
-        self.tree.heading("Name", text="Name")
-        self.tree.column("Description")
-        self.tree.heading("Description", text="Description")
-        self.tree.column("Num", width=50)
-        self.tree.heading("Num", text="Num")
-        self.frame.pack()
-        self.scrollbar.pack(side=RIGHT, fill=Y, expand=1)
-        self.tree.pack()
-        
+        self.grouper.set_model(GrouperModelSqlite3())
         self.view_storage = ViewStorageTk()
-        self.view_storage.set_widget(self.tree)
+        self.view_storage.set_widget(StorageInfoTable(root))
         self.grouper.view_storage = self.view_storage
 
     def open_storage(self):
