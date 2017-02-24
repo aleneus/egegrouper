695d45889d0fee51761e8addfbb210813c8a125c
aleneus <aleneus@gmail.com>
2017-02-24 23:02:18 +0300

    tk: separate windows

--- tkgrouper.py
+++ tkgrouper.py
@@ -47,20 +47,13 @@ class GroupInfoTable(TableWidget):
 #################################################################
        
 
-# Application
-
-class Application(Frame):
-    """Application class."""
-
-    def __init__(self, master=None):
+class FrameStorageInfo(Frame):
+    
+    def __init__(self, controller, master=None):
         super().__init__(master)
-        self.create_widgets()
-        self.init_mvc()
-
-    def create_widgets(self):
-        """Create widgets."""
         
-        # main menu
+        self.controller = controller
+
         menubar = Menu(root)
         filemenu = Menu(menubar, tearoff=0)
         filemenu.add_command(label="Open storage", command=self.open_storage)
@@ -72,29 +65,15 @@ class Application(Frame):
         self.storage_info_table = StorageInfoTable(root)
         self.storage_info_table.pack(side=LEFT, fill=BOTH)
         self.storage_info_table.tkraise()
-        self.storage_info_table.tree.bind("<<TreeviewSelect>>", self.group_info) # TODO: Access to tree.
-        self.tk_focusNext()        
-        self.group_info_table = GroupInfoTable(root)
-        self.group_info_table.pack(side=LEFT, fill=BOTH)
-        self.group_info_table.tkraise()
-        self.group_info_table.tree.bind("<Double-1>", self.plot_exam) # TODO: Access to tree.
-        self.group_info_table.tree.bind("<Return>", self.plot_exam) # TODO: Access to tree.
+        self.storage_info_table.tree.bind("<Double-1>", self.group_info) # TODO: Access to tree.
+        self.storage_info_table.tree.bind("<Return>", self.group_info) # TODO: Access to tree.
 
-    def init_mvc(self):
-        """Initialization of MVC system."""
-        self.grouper = GrouperController()
-        self.grouper.set_model(GrouperModelSqlite3())
-        # views
-        self.grouper.view_storage = ViewTableTk()
-        self.grouper.view_storage.set_widget(self.storage_info_table)
-        
-        self.grouper.view_group = ViewTableTk()
-        self.grouper.view_group.set_widget(self.group_info_table)
-
-        self.grouper.view_exam_plot = ViewExamPlot()
+        self.controller.view_storage = ViewTableTk()
+        self.controller.view_storage.set_widget(self.storage_info_table)
 
     def open_storage(self):
         """Open storage."""
+        pass
         self.file_opt = options = {}
         options['defaultextension'] = '.sme.sqlite'
         options['filetypes'] = [('all files', '.*'), ('sme db files', '.sme.sqlite')]
@@ -104,32 +83,76 @@ class Application(Frame):
         file_name = filedialog.askopenfilename()
         if not file_name:
             return
-        self.grouper.open_or_create_storage(file_name)
-        self.grouper.storage_info()
+        self.controller.open_or_create_storage(file_name)
+        self.controller.storage_info()
 
     def group_info(self, event):
         """Get and show information about examination in selected group."""
         try:
             item = self.storage_info_table.tree.selection()[0]
-            self.grouper.group_info(self.storage_info_table.tree.item(item,"text"))
+            if not self.child_opened():
+                self.window_second = Toplevel(self.master)
+                self.frame_second = FrameGroupInfo(self, self.window_second)
+                self.set_child_opened(True)
+                self.controller.view_group = ViewTableTk()
+                self.controller.view_group.set_widget(self.frame_second.get_widget())
+            self.controller.group_info(self.storage_info_table.tree.item(item,"text"))
         except IndexError:
             pass
+
+    def close_db_and_exit(self):
+        """Close data base and exit."""
+        self.controller.close_storage()
+        root.quit()
+
+    _child_opened = False
+    
+    def set_child_opened(self, value):
+        self._child_opened = value
+
+    def child_opened(self):
+        return self._child_opened
+
+    
+class FrameGroupInfo(Frame):
+    def __init__(self, parent, master=None):
+        super().__init__(master)
+        self.parent = parent
+        self.pack()
+        
+        self.master.protocol('WM_DELETE_WINDOW', self.on_destroy)
+        self.group_info_table = GroupInfoTable(self.master)
+        self.group_info_table.pack(side=LEFT, fill=BOTH)
+        self.group_info_table.tkraise()
+        self.group_info_table.tree.bind("<Double-1>", self.plot_exam) # TODO: Access to tree.
+        self.group_info_table.tree.bind("<Return>", self.plot_exam) # TODO: Access to tree.
+
+        self.parent.controller.view_group = ViewTableTk()
+        self.parent.controller.view_group.set_widget(self.group_info_table)
+        self.parent.controller.view_exam_plot = ViewExamPlot()
 
     def plot_exam(self, event):
         """Plot examination in separate matplotlib window."""
         try:
             item = self.group_info_table.tree.selection()[0]
-            self.grouper.plot_exam(self.group_info_table.tree.item(item,"text"))
+            self.parent.controller.plot_exam(self.group_info_table.tree.item(item,"text"))
         except IndexError:
             pass
         
-    def close_db_and_exit(self):
-        """Close data base and exit."""
-        self.grouper.close_storage()
-        Tk().quit()
+    def on_destroy(self):
+        self.master.destroy()
+        self.parent.set_child_opened(False)
 
+    def get_widget(self):
+        return self.group_info_table
 
 if __name__ == '__main__':
     root = Tk()
-    app = Application(root)
-    app.mainloop()
+    model = GrouperModelSqlite3()
+    controller = GrouperController()
+    controller.set_model(model)
+    
+    frame_storage = FrameStorageInfo(controller, root)
+    frame_storage.controller = controller
+    
+    frame_storage.mainloop()
