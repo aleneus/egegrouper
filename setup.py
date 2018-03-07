99496ec30f4e5db06c454346ee12954d72b2030b
aleneus <aleneus@gmail.com>
2018-03-07 12:33:44 +0300

    Refactoring of setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # EGEGrouper - Software for grouping electrogastroenterography examinations.
 
-# Copyright (C) 2017 Aleksandr Popov
+# Copyright (C) 2017-2018 Aleksandr Popov
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,45 +18,70 @@
 from setuptools import setup
 import os
 
+from egegrouper.glob import *
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(
-    name="EGEGrouper",
-    version="0.5.0",
-    description="Tool for grouping EGEG examinations",
-    author="Aleksandr Popov",
-    author_email="aleneus@gmail.com",
-    license = "GPLv3",
-    keywords = "electrophysiology electrogastrography electrogastroenterography biosignal storage dataset",
-    url = "https://bitbucket.org/aleneus/egegrouper",
-    long_description=read('README'),
-    packages=['egegrouper'],
-    install_requires = [
-        'numpy',
-        'matplotlib',
-        'tabulate',
-        'nose',
-        'pyreadline;platform_system=="Windows"',
+name="EGEGrouper"
+description="Tool for grouping EGEG examinations"
+author="Aleksandr Popov"
+author_email="aleneus@gmail.com"
+license = "GPLv3"
+keywords = "electrophysiology electrogastrography electrogastroenterography biosignal storage dataset"
+url = "https://bitbucket.org/aleneus/egegrouper"
+packages=['egegrouper']
+
+install_requires = [
+    'numpy',
+    'matplotlib',
+    'tabulate',
+    'nose',
+    'pyreadline;platform_system=="Windows"',
+]
+
+entry_points={
+    'console_scripts': [
+        'igrouper = egegrouper.igrouper:main'
     ],
-    entry_points={
-        'console_scripts': [
-            'igrouper = egegrouper.igrouper:main'
-        ],
-        'gui_scripts': [
-            'tkgrouper = egegrouper.tkgrouper:main'
-        ]
-    },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Environment :: X11 Applications",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-    ],
-)
+    'gui_scripts': [
+        'tkgrouper = egegrouper.tkgrouper:main'
+    ]
+}
+
+classifiers=[
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Environment :: X11 Applications",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Microsoft :: Windows",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+
+def linux_setup():
+    setup(
+        name=name,
+        version=VERSION,
+        description=description,
+        author=author,
+        author_email=author_email,
+        license = license,
+        keywords = keywords,
+        url = url,
+        long_description=read('README'),
+        packages=packages,
+        install_requires = install_requires,
+        entry_points=entry_points,
+        classifiers=classifiers,
+    )
+
+def windows_setup():
+    pass
+
+linux_setup()
+#from cx_Freeze import setup, Executable
+#windows_setup()
